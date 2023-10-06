8a3d12a23a3ff646ee7f6cd7722a59953d33f0d2
Lee Leahy <lpleahyjr@gmail.com>
2023-10-06 09:36:35 -1000

    OTA: Start and stop the network

--- Firmware/RTK_Everywhere/settings.h
+++ Firmware/RTK_Everywhere/settings.h
@@ -183,12 +183,13 @@ enum NetworkStates
 // Define the network users
 enum NetworkUsers
 {
-    NETWORK_USER_NTP_SERVER = 0,   // NTP server
-    NETWORK_USER_NTRIP_CLIENT,     // NTRIP client
-    NETWORK_USER_NTRIP_SERVER,     // NTRIP server
-    NETWORK_USER_PVT_CLIENT,       // PVT client
-    NETWORK_USER_PVT_SERVER,       // PVT server
-    NETWORK_USER_PVT_UDP_SERVER,   // PVT UDP server
+    NETWORK_USER_NTP_SERVER = 0,        // NTP server
+    NETWORK_USER_NTRIP_CLIENT,          // NTRIP client
+    NETWORK_USER_NTRIP_SERVER,          // NTRIP server
+    NETWORK_USER_OTA_FIRMWARE_UPDATE,   // Over-The-Air firmware updates
+    NETWORK_USER_PVT_CLIENT,            // PVT client
+    NETWORK_USER_PVT_SERVER,            // PVT server
+    NETWORK_USER_PVT_UDP_SERVER,        // PVT UDP server
     // Last network user
     NETWORK_USER_MAX
 };
@@ -1100,6 +1101,7 @@ struct struct_online
     ethernetStatus_e ethernetStatus = ETH_NOT_STARTED;
     bool ethernetNTPServer = false; // EthernetUDP
     bool imu = false;
+    bool otaFirmwareUpdate = false;
 } online;
 
 #ifdef COMPILE_WIFI
