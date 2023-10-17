1818a97dbebd5ed5e9ae5c3cb066a8125cb387c7
Lee Leahy <lpleahyjr@gmail.com>
2023-10-17 07:36:10 -1000

    Fix compiles with and without COMPILE_ETHERNET and COMPILE_WIFI

--- Firmware/RTK_Everywhere/Developer.ino
+++ Firmware/RTK_Everywhere/Developer.ino
@@ -131,7 +131,6 @@ void pvtServerUpdate() {}
 void pvtServerZeroTail() {}
 void pvtServerValidateTables() {}
 void discardPvtServerBytes(RING_BUFFER_OFFSET previousTail, RING_BUFFER_OFFSET newTail) {}
-void discardPvtUdpServerBytes(RING_BUFFER_OFFSET previousTail, RING_BUFFER_OFFSET newTail) {}
 
 //----------------------------------------
 // WiFi
--- Firmware/RTK_Everywhere/RTK_Everywhere.ino
+++ Firmware/RTK_Everywhere/RTK_Everywhere.ino
@@ -214,8 +214,11 @@ char logFileName[sizeof("SFE_Reference_Station_230101_120101.ubx_plusExtraSpace"
 // Over-the-Air (OTA) update support
 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
+#if COMPILE_NETWORK
 #include <SSLClientESP32.h> // http://librarymanager/All#SSLClientESP32
 #include "X509_Certificate_Bundle.h" // Root certificates
+#endif // COMPILE_NETWORK
+#include <ArduinoJson.h>  //http://librarymanager/All#Arduino_JSON_messagepack v6.19.4
 
 #include "esp_ota_ops.h" //Needed for partition counting and updateFromSD
 
@@ -241,9 +244,8 @@ unsigned int binBytesSent = 0;                // Tracks firmware bytes sent over
 // Connection settings to NTRIP Caster
 //=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 #ifdef COMPILE_WIFI
-#include <ArduinoJson.h> //http://librarymanager/All#Arduino_JSON_messagepack v6.19.4
-#include <ESPmDNS.h>     //Built-in.
-#include <HTTPClient.h>  //Built-in. Needed for ThingStream API for ZTP
+#include <ESPmDNS.h>      //Built-in.
+#include <HTTPClient.h>   //Built-in. Needed for ThingStream API for ZTP
 #include <PubSubClient.h> //http://librarymanager/All#PubSubClient_MQTT_Lightweight by Nick O'Leary v2.8.0 Used for MQTT obtaining of keys
 #include <WiFi.h>             //Built-in.
 #include <WiFiClientSecure.h> //Built-in.
