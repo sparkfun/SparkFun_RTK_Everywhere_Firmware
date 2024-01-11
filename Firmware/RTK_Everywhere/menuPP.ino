adb88daf2a4839a4d956c18cdb790cc412a7337b
Lee Leahy <lpleahyjr@gmail.com>
2024-01-10 18:05:43 -1000

    NVM: Return status from loadFile

--- Firmware/RTK_Everywhere/menuPP.ino
+++ Firmware/RTK_Everywhere/menuPP.ino
@@ -430,7 +430,7 @@ bool checkCertificates()
 
     // Load the certificate
     memset(certificateContents, 0, MQTT_CERT_SIZE);
-    loadFile("certificate", certificateContents);
+    loadFile("certificate", certificateContents, settings.debugPpCertificate);
 
     if (checkCertificateValidity(certificateContents, strlen(certificateContents)) == false)
     {
@@ -441,7 +441,7 @@ bool checkCertificates()
 
     // Load the private key
     memset(keyContents, 0, MQTT_CERT_SIZE);
-    loadFile("privateKey", keyContents);
+    loadFile("privateKey", keyContents, settings.debugPpCertificate);
 
     if (checkPrivateKeyValidity(keyContents, strlen(keyContents)) == false)
     {
@@ -555,12 +555,12 @@ bool pointperfectUpdateKeys()
 
         // Get the certificate
         memset(certificateContents, 0, MQTT_CERT_SIZE);
-        loadFile("certificate", certificateContents);
+        loadFile("certificate", certificateContents, false);
         secureClient.setCertificate(certificateContents);
 
         // Get the private key
         memset(keyContents, 0, MQTT_CERT_SIZE);
-        loadFile("privateKey", keyContents);
+        loadFile("privateKey", keyContents, false);
         secureClient.setPrivateKey(keyContents);
 
         secureClient.setCACert(AWS_PUBLIC_CERT);
