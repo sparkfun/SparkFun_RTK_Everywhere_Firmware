24ca40b928c822a9fdb307a3b57831c26e4b8fac
Nathan Seidle <nathan@sparkfun.com>
2024-02-04 12:38:14 -0700

    Remove duplicate define

--- Firmware/RTK_Everywhere/Begin.ino
+++ Firmware/RTK_Everywhere/Begin.ino
@@ -142,9 +142,6 @@ void beginBoard()
         pin_I2C0_SDA = 15;
         pin_I2C0_SCL = 4;
 
-        pin_I2C0_SDA = 15;
-        pin_I2C0_SCL = 4;
-
         pin_GnssUart_RX = 26;
         pin_GnssUart_TX = 27;
         pin_GNSS_DR_Reset = 22; // Push low to reset GNSS/DR.
