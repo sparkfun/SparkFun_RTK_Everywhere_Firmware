ed275b68eedcbe9eb1915af9ca02f82e11062f75
Nathan Seidle <nathan@sparkfun.com>
2024-02-10 16:49:32 -0700

    Remove bluetoothStops

--- Firmware/RTK_Everywhere/menuFirmware.ino
+++ Firmware/RTK_Everywhere/menuFirmware.ino
@@ -22,7 +22,6 @@ static const int otaStateEntries = sizeof(otaStateNames) / sizeof(otaStateNames[
 // Locals
 //----------------------------------------
 
-static bool otaBluetoothOnline;
 static uint32_t otaLastUpdateCheck;
 static OtaState otaState;
 
@@ -98,8 +97,6 @@ void menuFirmware()
             }
             else
             {
-                bluetoothStop(); // Stop Bluetooth to allow for SSL on the heap
-
                 // Attempt to connect to local WiFi
                 if (wifiConnect(10000) == true)
                 {
@@ -132,11 +129,6 @@ void menuFirmware()
                 {
                     bool previouslyConnected = wifiIsConnected();
 
-                    // Stop Bluetooth to allow for SSL on the heap
-                    bool bluetoothOriginallyOnline = online.bluetooth;
-                    if (bluetoothOriginallyOnline)
-                        bluetoothStop();
-
                     // Attempt to connect to local WiFi
                     if (wifiConnect(10000) == true)
                     {
@@ -167,9 +159,6 @@ void menuFirmware()
 
                     if (previouslyConnected == false)
                         WIFI_STOP();
-
-                    if (bluetoothOriginallyOnline == true)
-                        bluetoothStart(); // Restart BT according to settings
                 }
             } // End wifiNetworkCount() check
         }
@@ -894,14 +883,6 @@ void otaAutoUpdateStop()
         // Stop the firmware update
         otaSetState(OTA_STATE_OFF);
         otaLastUpdateCheck = millis();
-
-        // Restart bluetooth if necessary
-        if (otaBluetoothOnline)
-        {
-            if (settings.debugFirmwareUpdate)
-                systemPrintln("Firmware update restarting Bluetooth");
-            bluetoothStart(); // Restart BT according to settings
-        }
     }
 };
 
@@ -963,15 +944,6 @@ void otaAutoUpdate()
             {
                 if (settings.debugFirmwareUpdate)
                     systemPrintln("Firmware update connected to WiFi");
-
-                // Stop Bluetooth
-                otaBluetoothOnline = online.bluetooth;
-                if (otaBluetoothOnline)
-                {
-                    if (settings.debugFirmwareUpdate)
-                        systemPrintln("Firmware update stopping Bluetooth");
-                    bluetoothStop();
-                }
 
                 // Get the latest firmware version
                 otaSetState(OTA_STATE_GET_FIRMWARE_VERSION);
