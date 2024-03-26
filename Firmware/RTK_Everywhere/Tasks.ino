963458dad319772260e0136aab358caedafc0def
PaulZC <pc235603@gmail.com>
2024-03-26 08:19:44 +0000

    Fix display_type for Torch

--- Firmware/RTK_Everywhere/Tasks.ino
+++ Firmware/RTK_Everywhere/Tasks.ino
@@ -1268,7 +1268,7 @@ void buttonCheckTask(void *e)
 
         userBtn->read();
 
-        if (present.imu_im19 && (present.display_type < DISPLAY_MAX_NONE))
+        if (present.imu_im19 && (present.display_type == DISPLAY_MAX_NONE))
         {
             // Platform has no display and tile, ie RTK Torch
 
