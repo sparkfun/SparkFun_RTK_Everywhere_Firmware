95f382f9b6dca1d55409f893ed2992a0924126bb
PaulZC <pc235603@gmail.com>
2024-05-20 11:04:49 +0100

    Update espnowOnDataReceived

--- Firmware/RTK_Everywhere/ESPNOW.ino
+++ Firmware/RTK_Everywhere/ESPNOW.ino
@@ -35,7 +35,7 @@ void espnowOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 #endif // COMPILE_ESPNOW
 
 // Callback when data is received
-void espnowOnDataReceived(const uint8_t *mac, const uint8_t *incomingData, int len)
+void espnowOnDataReceived(const esp_now_recv_info *mac, const uint8_t *incomingData, int len)
 {
 #ifdef COMPILE_ESPNOW
     if (espnowState == ESPNOW_PAIRING)
