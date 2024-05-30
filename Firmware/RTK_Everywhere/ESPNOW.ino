451c0693391c733a6907237a350bd3cfeadaa39e
PaulZC <pc235603@gmail.com>
2024-05-30 17:05:38 +0100

    Fix for esp_now_recv_info when COMPILE_ESPNOW is not defined

--- Firmware/RTK_Everywhere/ESPNOW.ino
+++ Firmware/RTK_Everywhere/ESPNOW.ino
@@ -34,6 +34,10 @@ void espnowOnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 }
 #endif // COMPILE_ESPNOW
 
+#ifndef COMPILE_ESPNOW
+#define esp_now_recv_info uint8_t
+#endif
+
 // Callback when data is received
 void espnowOnDataReceived(const esp_now_recv_info *mac, const uint8_t *incomingData, int len)
 {
