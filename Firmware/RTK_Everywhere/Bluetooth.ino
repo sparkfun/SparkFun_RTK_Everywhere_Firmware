101c03b11d7d8cbd27784311a6099015a73fd109
PaulZC <pc235603@gmail.com>
2024-06-03 08:02:54 +0100

    Add 4-digit PIN warning

--- Firmware/RTK_Everywhere/Bluetooth.ino
+++ Firmware/RTK_Everywhere/Bluetooth.ino
@@ -315,6 +315,8 @@ void bluetoothStart()
         // See issue: https://github.com/sparkfun/SparkFun_RTK_Firmware/issues/5
         // https://github.com/espressif/esp-idf/issues/1541
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+        /*
+        // Note: Since version 3.0.0 this library does not support legacy pairing (using fixed PIN consisting of 4 digits).
         esp_bt_sp_param_t param_type = ESP_BT_SP_IOCAP_MODE;
 
         esp_bt_io_cap_t iocap = ESP_BT_IO_CAP_NONE; // Requires pin 1234 on old BT dongle, No prompt on new BT dongle
@@ -330,6 +332,7 @@ void bluetoothStart()
         pin_code[2] = '3';
         pin_code[3] = '4';
         esp_bt_gap_set_pin(pin_type, 4, pin_code);
+        */
         //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
         if (settings.bluetoothRadioType == BLUETOOTH_RADIO_SPP_AND_BLE)
