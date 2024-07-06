7df152010a035f61f5a1bdb5cd05272aa061db99
PaulZC <pc235603@gmail.com>
2024-07-06 11:11:16 +0100

    Create TCP_Client.py

---
+++ Firmware/Tools/TCP_Client.py
@@ -0,0 +1,34 @@
+import socket
+import argparse
+import multiprocessing
+
+def client(server, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((server, port))
+    while True:
+        payload = sock.recv(1024)
+        print(payload.decode(), end='')
+        
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description='TCP Client')
+
+    parser.add_argument('-server', type=str, default="",
+                        help='Host Name or IP Address of the TCP Server')
+
+    parser.add_argument('-port', type=int, default=2948,
+                        help='TCP Port Number')
+
+    args = parser.parse_args()
+
+    print("Connecting to " + args.server + " on port " + str(args.port))
+
+    proc = multiprocessing.Process(target = client, args = (args.server, args.port))
+    proc.start()
+
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        proc.terminate()
