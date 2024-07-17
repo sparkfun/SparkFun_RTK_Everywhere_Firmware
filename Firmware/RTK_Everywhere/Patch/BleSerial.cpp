59421084b3add61d1b730860f60203306a009d1b
Nathan Seidle <nathan@sparkfun.com>
2024-07-17 16:56:58 -0600

    Allow RX data over BLE
    Fix issue #466

--- Firmware/RTK_Everywhere/Patch/BleSerial.cpp
+++ Firmware/RTK_Everywhere/Patch/BleSerial.cpp
@@ -164,7 +164,7 @@ void BleSerial::onWrite(BLECharacteristic *pCharacteristic)
 {
 	if (pCharacteristic->getUUID().toString() == BLE_RX_UUID)
 	{
-		String value = pCharacteristic->getValue().c_str();
+		String value = pCharacteristic->getValue();
 		for (int i = 0; i < value.length(); i++)
 			receiveBuffer.add(value[i]);
 	}
