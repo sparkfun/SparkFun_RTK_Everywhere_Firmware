53631dc25ffa9a73fbb045b6253d4e5661cf988e
Nathan Seidle <nathan@sparkfun.com>
2024-08-29 13:10:10 -0600

    Update last attempt only if RTC is online
    Re-implementing main #422 - https://github.com/sparkfun/SparkFun_RTK_Everywhere_Firmware/commit/09a4b45e505c6ac9d62ffa463419e57630712169

--- Firmware/RTK_Everywhere/MQTT_Client.ino
+++ Firmware/RTK_Everywhere/MQTT_Client.ino
@@ -463,9 +463,10 @@ void mqttClientReceiveMessage(int messageSize)
                 settings.pointPerfectNextKeyDuration =
                     (1000LL * 60 * 60 * 24 * 28) - 1; // Assume next key duration is 28 days
 
-                settings.lastKeyAttempt = rtc.getEpoch(); // Mark it
-                
-                recordSystemSettings();                   // Record these settings to unit
+                if (online.rtc)
+                    settings.lastKeyAttempt = rtc.getEpoch(); // Mark it - but only if RTC is online
+                    
+                recordSystemSettings();                       // Record these settings to unit
 
                 if (settings.debugCorrections == true)
                     pointperfectPrintKeyInformation();
