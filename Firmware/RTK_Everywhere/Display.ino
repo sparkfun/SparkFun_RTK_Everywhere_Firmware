80dc15787eebdd19c242df598756374eb098a7de
Lee Leahy <lpleahyjr@gmail.com>
2025-02-17 15:21:38 -1000

    Display: Add displayEthernetIcon routine

--- Firmware/RTK_Everywhere/Display.ino
+++ Firmware/RTK_Everywhere/Display.ino
@@ -3052,6 +3052,20 @@ void displayWebConfigNotStarted()
     displayMessage("Web Config", 0);
 }
 
+int displayEthernetIcon()
+{
+    static bool blink;
+    uint8_t xPos = (oled->getWidth() - Ethernet_Icon_Width) / 2;
+    int yPos = Ethernet_Icon_Height / 2; // yPos is 6
+
+    blink ^= 1;
+    if (ETH.linkUp() || blink)
+        displayBitmap(xPos, yPos, Ethernet_Icon_Width, Ethernet_Icon_Height, Ethernet_Icon);
+
+    yPos += Ethernet_Icon_Height * 1.5; // yPos is now 24
+    return yPos;
+}
+
 void displayConfigViaWiFi()
 {
     // Characters before pixels start getting cut off. 11 characters can cut off a few pixels.
