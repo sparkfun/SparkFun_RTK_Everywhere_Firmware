233beff894b6f96947683e556640fde233d7eb45
Lee Leahy <lpleahyjr@gmail.com>
2025-04-15 11:54:51 -1000

    Display: Add displayHalt routine, call from reportFatalError
    Move verifyTables to later in the boot sequence to enable HALT display

--- Firmware/RTK_Everywhere/RTK_Everywhere.ino
+++ Firmware/RTK_Everywhere/RTK_Everywhere.ino
@@ -1073,9 +1073,6 @@ void setup()
     systemPrintln();
     systemPrintln();
 
-    DMW_b("verifyTables");
-    verifyTables(); // Verify the consistency of the internal tables
-
     DMW_b("findSpiffsPartition");
     if (!findSpiffsPartition())
     {
@@ -1129,6 +1126,9 @@ void setup()
 
     DMW_b("beginDisplay");
     beginDisplay(i2cDisplay); // Start display to be able to display any errors
+
+    DMW_b("verifyTables");
+    verifyTables(); // Verify the consistency of the internal tables
 
     beginVersion(); // Assemble platform name. Requires settings/LFS.
 
