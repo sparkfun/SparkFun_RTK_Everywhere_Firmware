979309df3412b9e16abd0faab3e02a481785029b
PaulZC <pc235603@gmail.com>
2025-07-24 15:52:41 +0100

    Remove redundant RTCM parsing

--- Firmware/RTK_Everywhere/MQTT_Client.ino
+++ Firmware/RTK_Everywhere/MQTT_Client.ino
@@ -514,7 +514,7 @@ int mqttClientProcessZedMessage(uint8_t *mqttData, uint16_t mqttCount, int bytes
             zed->updateCorrectionsSource(0); // Set SOURCE to 0 (IP) if needed
 
             gnss->pushRawData(mqttData, mqttCount);
-            sempParseNextBytes(rtcmParse, mqttData, mqttCount); // Parse the data for RTCM1005/1006
+            // Corrections are SPARTN. No point in pushing them to rtcmParse
             bytesPushed += mqttCount;
 
             mqttClientDataReceived = true;
@@ -543,7 +543,7 @@ int mqttClientProcessZedMessage(uint8_t *mqttData, uint16_t mqttCount, int bytes
         }
 
         gnss->pushRawData(mqttData, mqttCount);
-        sempParseNextBytes(rtcmParse, mqttData, mqttCount); // Parse the data for RTCM1005/1006
+        // No point in pushing keys / MGA to rtcmParse
         bytesPushed += mqttCount;
     }
 #endif // COMPILE_ZED
