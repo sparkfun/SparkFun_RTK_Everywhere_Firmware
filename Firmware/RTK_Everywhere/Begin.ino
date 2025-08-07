534dc6bae61f3c967233f30cc2a62358dcf249dc
Nathan Seidle <nathan@sparkfun.com>
2025-08-07 11:24:40 -0600

    Fix typo

--- Firmware/RTK_Everywhere/Begin.ino
+++ Firmware/RTK_Everywhere/Begin.ino
@@ -1314,7 +1314,7 @@ void beginButtons()
     {
         if (beginGpioExpander(0x20) == false)
         {
-            systemPrintln("Directional pad not detected");
+            systemPrintln("Portability Shield not detected");
 
             present.microSd = false;
 
@@ -1603,7 +1603,7 @@ bool i2cBusInitialization(TwoWire *i2cBus, int sda, int scl, int clockKHz)
             }
 
             case 0x10: {
-                systemPrintf("  0x%02X - MFI343S00177 Authenication Coprocessor\r\n", addr);
+                systemPrintf("  0x%02X - MFI343S00177 Authentication Coprocessor\r\n", addr);
                 i2cAuthCoPro = i2cBus; // Record the bus
                 break;
             }
