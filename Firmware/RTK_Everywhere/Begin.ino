sparkfun/SparkFun_RTK_Everywhere_Firmware

commit 62fe87002b556cd25de430aa6f0c031adb3d5940

Add present.mosaicMicroSd, mosaicSdCardSize, mosaicSdFreeSpace

Firmware/RTK_Everywhere/Begin.ino
@@ -652,6 +652,7 @@ void beginBoard()
         present.invertedFastPowerOff = true;
         present.gnss_to_uart = true;
         present.gnss_to_uart2 = true;
+        present.mosaicMicroSd;
         present.microSdCardDetectLow = true; // Except microSD is connected to mosaic... present.microSd is false
 
         present.minCno = true;

Firmware/RTK_Everywhere/GNSS.ino
@@ -198,6 +198,7 @@ void gnssDetectReceiverType()
         present.minCno = true;
         present.minElevation = true;
         present.dynamicModel = true;
+        present.mosaicMicroSd = true;
         // present.needsExternalPpl = true; // Nope. No L-Band support...
 
 #endif // COMPILE_MOSAICX5

Firmware/RTK_Everywhere/GNSS_Mosaic.ino
@@ -31,25 +31,54 @@ GNSS_Mosaic.ino
 //----------------------------------------
 void menuLogMosaic()
 {
+    if (!present.mosaicMicroSd) // This may be needed for the G5 P3 ?
+        return;
+
     bool applyChanges = false;
 
     while (1)
     {
         systemPrintln();
         systemPrintln("Menu: Logging");
+        systemPrintln();
 
         char sdCardSizeChar[20];
         String cardSize;
-        stringHumanReadableSize(cardSize, sdCardSize);
+        stringHumanReadableSize(cardSize, mosaicSdCardSize);
         cardSize.toCharArray(sdCardSizeChar, sizeof(sdCardSizeChar));
         char sdFreeSpaceChar[20];
         String freeSpace;
-        stringHumanReadableSize(freeSpace, sdFreeSpace);
+        stringHumanReadableSize(freeSpace, mosaicSdFreeSpace);
         freeSpace.toCharArray(sdFreeSpaceChar, sizeof(sdFreeSpaceChar));
 
+        // On Facet mosaic, the SD is connected directly to the X5 and is accessible
+        // On Facet Flex X5, the internal mosaic SD card is not accessible
         char myString[70];
-        snprintf(myString, sizeof(myString), "SD card size: %s / Free space: %s", sdCardSizeChar, sdFreeSpaceChar);
+        snprintf(myString, sizeof(myString), "Mosaic SD card size: %s / Free space: %s", sdCardSizeChar, sdFreeSpaceChar);
         systemPrintln(myString);
+        systemPrintln();
+
+        if (settings.enableSD && online.microSD)
+        {
+            char sdCardSizeChar[20];
+            String cardSize;
+            stringHumanReadableSize(cardSize, sdCardSize);
+            cardSize.toCharArray(sdCardSizeChar, sizeof(sdCardSizeChar));
+            char sdFreeSpaceChar[20];
+            String freeSpace;
+            stringHumanReadableSize(freeSpace, sdFreeSpace);
+            freeSpace.toCharArray(sdFreeSpaceChar, sizeof(sdFreeSpaceChar));
+
+            char myString[70];
+            snprintf(myString, sizeof(myString), "Accessible SD card size: %s / Free space: %s", sdCardSizeChar, sdFreeSpaceChar);
+            systemPrintln(myString);
+
+            if (online.logging)
+            {
+                systemPrintf("Accessible SD current log file name: %s\r\n", logFileName);
+            }
+            systemPrintln();
+        }
 
         systemPrint("1) Log NMEA to microSD: ");
         if (settings.enableLogging == true)
@@ -2651,6 +2680,9 @@ void GNSS_MOSAIC::storeBlock4014(SEMP_PARSE_STATE *parse)
 //----------------------------------------
 void GNSS_MOSAIC::storeBlock4059(SEMP_PARSE_STATE *parse)
 {
+    if (!present.mosaicMicroSd)
+        return;
+    
     if (sempSbfGetU1(parse, 14) < 1) // Check N is at least 1
         return;
 
@@ -2670,9 +2702,15 @@ void GNSS_MOSAIC::storeBlock4059(SEMP_PARSE_STATE *parse)
 
     uint64_t diskUsage = (diskUsageMSB * 4294967296) + diskUsageLSB;
 
-    sdCardSize = diskSizeMB * 1048576; // Convert to bytes
+    mosaicSdCardSize = diskSizeMB * 1048576; // Convert to bytes
 
-    sdFreeSpace = sdCardSize - diskUsage;
+    mosaicSdFreeSpace = mosaicSdCardSize - diskUsage;
+
+    if (!present.microSd) // Overwrite - if this is the only SD card
+    {
+        sdCardSize = mosaicSdCardSize;
+        sdFreeSpace = mosaicSdFreeSpace;
+    }
 
     _diskStatusSeen = true;
 }

Firmware/RTK_Everywhere/RTK_Everywhere.ino
@@ -368,6 +368,8 @@ const char *ringBufferSemaphoreHolder = "None";
 // Display used/free space in menu and config page
 uint64_t sdCardSize;
 uint64_t sdFreeSpace;
+uint64_t mosaicSdCardSize;
+uint64_t mosaicSdFreeSpace;
 bool outOfSDSpace;
 const uint32_t sdMinAvailableSpace = 10000000; // Minimum available bytes before SD is marked as out of space
 

Firmware/RTK_Everywhere/menuMain.ino
@@ -235,7 +235,7 @@ void menuMain()
         else if (incoming == 4)
             menuPorts();
 #ifdef COMPILE_MOSAICX5
-        else if (incoming == 5 && productVariant == RTK_FACET_MOSAIC)
+        else if (incoming == 5 && present.gnss_mosaicX5)
             menuLogMosaic();
 #endif                                                         // COMPILE_MOSAICX5
         else if (incoming == 5 && productVariant != RTK_TORCH) // Torch does not have logging

Firmware/RTK_Everywhere/settings.h
@@ -1871,6 +1871,7 @@ struct struct_present
     bool imu_zedf9r = false;
 
     bool microSd = false;
+    bool mosaicMicroSd = false;
     bool microSdCardDetectLow = false; // Card detect low = SD in place
     bool microSdCardDetectHigh = false; // Card detect high = SD in place
     bool microSdCardDetectGpioExpanderHigh = false; // Card detect on GPIO5, high = SD in place

commit 7c9cae155245b5370cba6ef035932b223dd0737b

Fix present.mosaicMicroSd
This has been broken since 62fe87002b556cd25de430aa6f0c031adb3d5940
I must have only ever tested it on Flex (X5). Sloppy...

Firmware/RTK_Everywhere/Begin.ino
@@ -657,7 +657,7 @@ void beginBoard()
         present.invertedFastPowerOff = true;
         present.gnss_to_uart = true;
         present.gnss_to_uart2 = true;
-        present.mosaicMicroSd;
+        present.mosaicMicroSd = true;
         present.microSdCardDetectLow = true; // Except microSD is connected to mosaic... present.microSd is false
 
         present.minCno = true;