93b2244e60b0ba394e9c31c8a9ba06279ecb9f1a
Paul Clark <pc235603@gmail.com>
2026-01-12 11:21:37 +0000

    Copy partitions.bin

--- Firmware/compile_with_docker.bat
+++ Firmware/compile_with_docker.bat
@@ -6,4 +6,5 @@ docker create --name=rtk_everywhere rtk_everywhere_firmware:latest
 docker cp rtk_everywhere:/RTK_Everywhere.ino.bin .
 docker cp rtk_everywhere:/RTK_Everywhere.ino.elf .
 docker cp rtk_everywhere:/RTK_Everywhere.ino.bootloader.bin .
+docker cp rtk_everywhere:/RTK_Everywhere.ino.partitions.bin .
 docker container rm rtk_everywhere
