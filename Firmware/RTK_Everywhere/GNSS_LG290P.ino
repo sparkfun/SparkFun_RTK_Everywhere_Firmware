e087bcf1d477fed564c19642547fba666b6311f0
Paul Clark <pc235603@gmail.com>
2026-06-19 09:22:28 -0600

    Update GNSS_LG290P.ino

--- Firmware/RTK_Everywhere/GNSS_LG290P.ino
+++ Firmware/RTK_Everywhere/GNSS_LG290P.ino
@@ -2005,14 +2005,12 @@ bool GNSS_LG290P::setConstellations()
 
     if (online.gnss)
     {
-        response &= _lg290p->setConstellations(settings.lg290pConstellations[0],  // GPS
+        response = _lg290p->setConstellations(settings.lg290pConstellations[0],  // GPS
                                                settings.lg290pConstellations[1],  // GLONASS
                                                settings.lg290pConstellations[2],  // Galileo
                                                settings.lg290pConstellations[3],  // BDS
                                                settings.lg290pConstellations[4],  // QZSS
                                                settings.lg290pConstellations[5]); // NavIC
-        response &= _lg290p->setRtkDifferentialAge(settings.lg290pRtkDifferentialAge);
-        response &= _lg290p->setRtkDifferentialSourceType(settings.lg290pRtkDifferentialSourceType);
     }
 
     gnssConfigure(GNSS_CONFIG_RESET); // Constellation changes require device save/restart
