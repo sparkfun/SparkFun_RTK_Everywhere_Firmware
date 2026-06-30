cffe39bf43e7683fbfc9a9c8396801107148e7db
Lee Leahy <lpleahyjr@gmail.com>
2026-06-30 04:04:27 -1000

    Device_Update: if _doAll == true, don't reboot upon failure

--- Firmware/RTK_Everywhere/Device_Firmware_Update.ino
+++ Firmware/RTK_Everywhere/Device_Firmware_Update.ino
@@ -1003,7 +1003,8 @@ void deviceFirmwareReadFillBuffer(DEVICE_FIRMWARE_CTX * ctx, uint32_t currentMse
     if (deviceFirmwareRead(ctx, currentMsec, DFUS_DEVICE_CLOSE))
     {
         deviceFirmwareStopTasks(ctx);
-        ctx->_reboot = true;
+        if (ctx->_doAll == false)
+            ctx->_reboot = true;
         deviceFirmwareStateSet(ctx, DFUS_DEVICE_RESET);
     }
 }
