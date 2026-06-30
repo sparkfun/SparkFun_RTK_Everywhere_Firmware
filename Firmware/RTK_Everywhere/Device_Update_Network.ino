fdb83830b10b432df8fa6dc8f11d1bc0d6c1317c
Lee Leahy <lpleahyjr@gmail.com>
2026-06-30 07:54:42 -1000

    Network update: Breakup parsing to return errors

--- Firmware/RTK_Everywhere/Device_Update_Network.ino
+++ Firmware/RTK_Everywhere/Device_Update_Network.ino
@@ -191,6 +191,24 @@ void dfuNetworkFileListGetFileName(DEVICE_FIRMWARE_CTX * ctx, uint32_t currentMs
 }
 
 //----------------------------------------
+// Display the error message and exit
+//----------------------------------------
+void dfuNetworkFileListError(DEVICE_FIRMWARE_CTX * ctx, const char * message)
+{
+    DFU_BUFFER_DATA * bufferData;
+
+    // Display the error message
+    systemPrintf("ERROR: %s\r\n", message);
+
+    // Restore access to the data buffer
+    bufferData = &dfuFirmwareFileNamesNet;
+    dfuNetworkCleanup(ctx, bufferData);
+
+    // No files available
+    deviceFirmwareStateSet(ctx, DFUS_GET_NVM_FILE_LIST);
+}
+
+//----------------------------------------
 // Request the web page containing the file specifications
 //----------------------------------------
 void dfuNetworkFileListHtmlRequest(DEVICE_FIRMWARE_CTX * ctx,
@@ -203,40 +221,42 @@ void dfuNetworkFileListHtmlRequest(DEVICE_FIRMWARE_CTX * ctx,
     const char * fileListPrefix;
     int httpResponseCode;
 
-    // Connect to the remote web page
-    ctx->_startMsec = currentMsec;
-    ctx->_https = new HTTPClient;
-    ctx->_https->begin(ctx->_url);
-    ctx->_https->setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
-
-    // Send HTTP GET request
-    httpResponseCode = ctx->_https->GET();
-
-    // Display the error
-    if (httpResponseCode != 200)
+    do
     {
-        // Display the response
-        if (settings.debugFirmwareUpdate)
-            systemPrintf("HTTP Response code: %d (%s)\r\n",
-                         httpResponseCode,
-                         ctx->_https->errorToString(httpResponseCode));
+        // Connect to the remote web page
+        ctx->_startMsec = currentMsec;
+        ctx->_https = new HTTPClient;
+        ctx->_https->begin(ctx->_url);
+        ctx->_https->setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
 
-        // Done with the HTTP link
-        dfuNetworkCleanup(ctx, nullptr);
+        // Send HTTP GET request
+        httpResponseCode = ctx->_https->GET();
 
-        // Stop with known error or too many retries
-        if ((httpResponseCode != -1) || (ctx->_attemptNumber++ >= 3))
+        // Display the error
+        if (httpResponseCode != 200)
         {
-            systemPrintf("Failed to open url: %s\r\n", ctx->_url.c_str());
-            deviceFirmwareStateSet(ctx, DFUS_GET_NVM_FILE_LIST);
+            // Display the response
+            if (settings.debugFirmwareUpdate)
+                systemPrintf("HTTP Response code: %d (%s)\r\n",
+                             httpResponseCode,
+                             ctx->_https->errorToString(httpResponseCode));
+
+            // Done with the HTTP link
+            dfuNetworkCleanup(ctx, nullptr);
+
+            // Stop with known error or too many retries
+            if ((httpResponseCode != -1) || (ctx->_attemptNumber++ >= 3))
+            {
+                systemPrintf("Failed to open url: %s\r\n", ctx->_url.c_str());
+                deviceFirmwareStateSet(ctx, DFUS_GET_NVM_FILE_LIST);
+            }
+            else
+            {
+                // Retry accessing the web server
+            }
+            break;
         }
-        else
-        {
-            // Retry accessing the web server
-        }
-    }
-    else
-    {
+
         // Get TCP stream
         ctx->_networkClient = ctx->_https->getStreamPtr();
 
@@ -246,34 +266,35 @@ void dfuNetworkFileListHtmlRequest(DEVICE_FIRMWARE_CTX * ctx,
         ctx->_bufferLength = bufferData->_length;
         ctx->_validDataBytes = 0;
 
-        // Locate the beginning of the directory listing
+        // Get the parsing strings
         dirPrefix = ctx->_deviceInfo->_dirPrefix;
         dirSuffix = ctx->_deviceInfo->_dirSuffix;
         fileListPrefix = ctx->_deviceInfo->_dirPrefix2;
         filePrefix = ctx->_deviceInfo->_entryPrefix;
-        if ((dirPrefix && (ctx->_networkClient->find(dirPrefix) == false))
-            || (fileListPrefix && (ctx->_networkClient->find(fileListPrefix) == false)))
-        {
-            systemPrintf("ERROR: Directory listing not found!\r\n");
-            dfuNetworkCleanup(ctx, bufferData);
-            deviceFirmwareStateSet(ctx, DFUS_GET_NVM_FILE_LIST);
-        }
-        else
-        {
-            // Locate the first file name
-            if (ctx->_networkClient->findUntil(filePrefix, dirSuffix))
-                // Get the list of names
-                deviceFirmwareStateSet(ctx, DFUS_GET_NETWORK_FILE_LIST);
-            else
-            {
-                // Restore access to the data buffer
-                dfuNetworkCleanup(ctx, bufferData);
 
-                // No files available
-                deviceFirmwareStateSet(ctx, DFUS_GET_NVM_FILE_LIST);
-            }
+        // Locate the beginning of the directory listing
+        if (dirPrefix && (ctx->_networkClient->find(dirPrefix) == false))
+        {
+            dfuNetworkFileListError(ctx, "Directory prefix not found!");
+            break;
         }
-    }
+
+        if (fileListPrefix && (ctx->_networkClient->find(fileListPrefix) == false))
+        {
+            dfuNetworkFileListError(ctx, "File list prefix not found!");
+            break;
+        }
+
+        // Locate the first file name
+        if (ctx->_networkClient->findUntil(filePrefix, dirSuffix) == false)
+        {
+            dfuNetworkFileListError(ctx, "File not found!");
+            break;
+        }
+
+        // Get the list of files
+        deviceFirmwareStateSet(ctx, DFUS_GET_NETWORK_FILE_LIST);
+    } while (0);
 }
 
 //----------------------------------------
