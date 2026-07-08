c75bb4b76da1e33d57a393469c710138f19991ec
Nathan Seidle <nathan@sparkfun.com>
2026-07-08 16:08:15 -0600

    Examples: Only print progress if there is a change

--- Firmware/Test Sketches/Flash_Update/STM32_Update_From_Array/System.ino
+++ Firmware/Test Sketches/Flash_Update/STM32_Update_From_Array/System.ino
@@ -2,6 +2,7 @@
 void firmwareUpdateProgressCallback(uint16_t bytesProcessed)
 {
     const uint8_t progressBarWidth = 20;
+    static uint8_t lastUpdatePercent = 0;
 
     firmwareUpdateBytesProcessed += bytesProcessed;
 
@@ -14,6 +15,12 @@ void firmwareUpdateProgressCallback(uint16_t bytesProcessed)
 
     uint8_t filled = (progressPercent * progressBarWidth) / 100;
 
+        // Don't update unless there is a change
+    if (progressPercent == lastUpdatePercent)
+        return;
+
+    lastUpdatePercent = progressPercent;
+    
     Serial.print("Update Progress: [");
     for (uint8_t i = 0; i < progressBarWidth; i++)
         Serial.print(i < filled ? '#' : '-');
