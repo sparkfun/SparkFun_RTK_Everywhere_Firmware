b07bbdb9464b03bafb46e24224a3414408653dfb
Lee Leahy <lpleahyjr@gmail.com>
2026-07-13 06:02:26 -1000

    Tilt: Delete tilt sensor instance when tilt detection fails

--- Firmware/RTK_Everywhere/Tilt.ino
+++ Firmware/RTK_Everywhere/Tilt.ino
@@ -1219,6 +1219,8 @@ void tiltForceDetectionReboot()
 // Records outcome to NVM
 void tiltDetect()
 {
+    int x;
+
     // Only test housings that may have a tilt sensor on board
     if (variantHousingProperties->tiltPossible == false)
         return;
@@ -1270,7 +1272,7 @@ void tiltDetect()
     // The library will try twice with a 250ms
     // If communication fails, retry after a 3s timeout
     uint8_t maxTries = 2;
-    for (int x = 0; x < maxTries; x++)
+    for (x = 0; x < maxTries; x++)
     {
         if (tiltSensor->begin(SerialTiltTest) == true)
         {
@@ -1282,6 +1284,13 @@ void tiltDetect()
 
         if (x < (maxTries - 1))
             delay(3000);
+    }
+
+    // Check for tilt sensor not detected
+    if (x == maxTries)
+    {
+        delete tiltSensor;
+        tiltSensor = nullptr;
     }
 
     SerialTiltTest.end(); // Release UART2 for reuse
