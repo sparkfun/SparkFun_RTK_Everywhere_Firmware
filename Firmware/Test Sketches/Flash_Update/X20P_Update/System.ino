e3b829eb917ae789c251511ceb493ed127f90a36
Lee Leahy <lpleahyjr@gmail.com>
2026-08-22 07:42:19 -1000

    X20P Flash Update: Add dumpBuffer

--- Firmware/Test Sketches/Flash_Update/X20P_Update/System.ino
+++ Firmware/Test Sketches/Flash_Update/X20P_Update/System.ino
@@ -84,6 +84,52 @@ bool otaSecurelyConnectGitHub(WiFiClientSecure &client)
     return true;
 }
 
+// Dump a buffer in hex and ASCII
+void dumpBuffer(size_t offset, const uint8_t *buffer, size_t length)
+{
+    int bytes;
+    const uint8_t *end;
+    int index;
+
+    end = &buffer[length];
+    while (buffer < end)
+    {
+        // Determine the number of bytes to display on the line
+        bytes = end - buffer;
+        if (bytes > (16 - (offset & 0xf)))
+            bytes = 16 - (offset & 0xf);
+
+        // Display the offset
+        systemPrintf("0x%08lx: ", offset);
+
+        // Skip leading bytes
+        for (index = 0; index < (offset & 0xf); index++)
+            systemPrintf("   ");
+
+        // Display the data bytes
+        for (index = 0; index < bytes; index++)
+            systemPrintf("%02X ", buffer[index]);
+
+        // Separate the data bytes from the ASCII
+        for (; index < (16 - (offset & 0xf)); index++)
+            systemPrintf("   ");
+        systemPrintf(" ");
+
+        // Skip leading bytes
+        for (index = 0; index < (offset & 0xf); index++)
+            systemPrintf(" ");
+
+        // Display the ASCII values
+        for (index = 0; index < bytes; index++)
+            systemPrintf("%c", ((buffer[index] < ' ') || (buffer[index] >= 0x7f)) ? '.' : buffer[index]);
+        systemPrintf("\r\n");
+
+        // Set the next line of data
+        buffer += bytes;
+        offset += bytes;
+    }
+}
+
 // Get a string from the user
 String systemGetStringFromUser()
 {
