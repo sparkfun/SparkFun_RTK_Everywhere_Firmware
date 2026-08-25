a6eb4a36b6ff03ccb9498556f8eba8879e5d228e
Lee Leahy <lpleahyjr@gmail.com>
2026-08-25 03:25:13 -1000

    Remove IM19_Update_From_Array

--- Firmware/Test Sketches/Flash_Update/Array_And_SD/IM19_Update_From_Array/Tilt.h
+++
@@ -1,6 +0,0 @@
-enum Im19UpdateResult
-{
-    IM19_UPDATE_FAILED = 0,
-    IM19_UPDATE_SUCCESS,
-    IM19_UPDATE_RETRY, // lost frames (or no response) - caller should re-stream the source and call again
-};
